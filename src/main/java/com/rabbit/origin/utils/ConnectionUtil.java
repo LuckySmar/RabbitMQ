485bc1f16cabfecc8c98564caf01f67b4c37d21d
Anonymous <1670439995@qq.com>
2020-07-29 17:34:09 +0800

    更改虚拟主机为   /

--- src/main/java/com/rabbit/origin/utils/ConnectionUtil.java
+++ src/main/java/com/rabbit/origin/utils/ConnectionUtil.java
@@ -14,7 +14,7 @@ public class ConnectionUtil {
         factory.setPort(5672);
         //设置账号信息，用户名、密码、vhost
         //如果要使用VirtualHost需要现在管理工具（http://127.0.0.1:15672/）中创建一个VirtualHost
-        factory.setVirtualHost("/");    //注意这里默认情况下的VirtualHost为 /
+        factory.setVirtualHost("/");    //注意这里默认情况下的VirtualHost为
         factory.setUsername("guest");
         factory.setPassword("guest");
         // 通过工程获取连接
